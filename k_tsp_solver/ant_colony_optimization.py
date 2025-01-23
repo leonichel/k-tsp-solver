12b7e2ab15dfd83865bfbb3bb24cd836e44811e1
Leonichel <leonichelg@gmail.com>
2025-01-23 09:51:51 -0300

    test: ant colony optimization

--- k_tsp_solver/ant_colony_optimization.py
+++ k_tsp_solver/ant_colony_optimization.py
@@ -34,66 +34,82 @@ class AntColonyOptimization(Model):
     def _update_pheromone(self, pheromone: np.ndarray, solutions: List[Solution]):
         pheromone *= 1 - self.evaporation_rate
         for solution in solutions:
-            for i in range(len(solution.path) - 1):
-                current = solution.path[i]
-                next_node = solution.path[i + 1]
+            for i in range(len(solution.path_vertices) - 1):
+                current = solution.path_vertices[i]
+                next_node = solution.path_vertices[i + 1]
                 pheromone[current, next_node] += 1 / solution.path_length
                 pheromone[next_node, current] += 1 / solution.path_length
         return pheromone
     
     def _calculate_probability(self, instance: Instance, current: int, next_node: int, pheromone: np.ndarray) -> float:
         pheromone_factor = pheromone[current, next_node] ** self.alpha
-        distance_factor = 1 / instance.adjacency_matrix[current, next_node] ** self.beta
+        distance_factor = 1 / instance.get_distance(current, next_node) ** self.beta
         return pheromone_factor * distance_factor
     
     def _calculate_probabilities(self, instance: Instance, current: int, visited: np.ndarray, pheromone: np.ndarray) -> np.ndarray:
         unvisited = np.where(visited == False)[0]
-        probabilities = np.zeros(instance.n)
+        probabilities = np.zeros(instance.number_of_vertices)
         for node in unvisited:
             probabilities[node] = self._calculate_probability(instance, current, node, pheromone)
         return probabilities / np.sum(probabilities)
     
     def _select_next_node(self, instance: Instance, current: int, visited: np.ndarray, pheromone: np.ndarray) -> int:
         probabilities = self._calculate_probabilities(instance, current, visited, pheromone)
-        return np.random.choice(instance.n, p=probabilities)
+        return np.random.choice(instance.number_of_vertices, p=probabilities)
     
-    def _generate_solution(self, instance: Instance, pheromone: np.ndarray) -> Solution:
+    def _generate_solution(
+            self, 
+            instance: Instance, 
+            pheromone: np.ndarray, 
+            k_factor: KFactor, 
+            has_closed_cycle: bool
+        ) -> Solution:
         path = []
-        visited = np.zeros(instance.n, dtype=bool)
-        visited[0] = True
-        current = 0
+        visited = np.zeros(instance.number_of_vertices, dtype=bool)
+        first_edge = random.randint(0, instance.number_of_vertices - 1)
+        visited[first_edge] = True
+        current = first_edge
         path.append(current)
 
-        for _ in range(1, instance.n):
+        for _ in range(1, instance.number_of_vertices):
             next_node = self._select_next_node(instance, current, visited, pheromone)
-            visited[next_node] = True
+            visited.add(next_node)
             path.append(next_node)
             current = next_node
         
         path.append(0)
-        path_length = instance.calculate_path_length(path)
-        solution = Solution(path, path_length)
+        solution = Solution(instance=instance, path_vertices=path, k_factor=k_factor, has_closed_cycle=has_closed_cycle)
+        solution.get_path_edges()
+        solution.evaluate_edge_path_length()
+        path_length = solution.path_length
+
         if self.best_solution is None or solution.path_length < self.best_path_length:
             self.best_solution = solution
             self.best_path_length = solution.path_length
         return solution
     
-    def _generate_solutions(self, instance: Instance, pheromone: np.ndarray) -> List[Solution]:
+    def _generate_solutions(
+            self, 
+            instance: Instance, 
+            pheromone: np.ndarray,  
+            k_factor: KFactor, 
+            has_closed_cycle: bool
+        ) -> List[Solution]:
         solutions = []
         for _ in range(self.ants):
-            solution = self._generate_solution(instance, pheromone)
+            solution = self._generate_solution(instance, pheromone, k_factor, has_closed_cycle)
             solutions.append(solution)
         return solutions
 
-    def _debug(self, instance: Instance, pheromone: np.ndarray):
-        logger.debug(f"pheromone: {pheromone}")
-        for i in range(self.ants):
-            solution = self._generate_solution(instance, pheromone)
-            logger.debug(f"ant {i}: {solution.path} {solution.path_length}")
-        logger.debug(f"best solution: {self.best_solution.path} {self.best_solution.path_length}")
-        logger.debug(f"best path length: {self.best_path_length}")
-        logger.debug("")
-        return pheromone
+    # def _debug(self, instance: Instance, pheromone: np.ndarray):
+    #     logger.debug(f"pheromone: {pheromone}")
+    #     for i in range(self.ants):
+    #         solution = self._generate_solution(instance, pheromone)
+    #         logger.debug(f"ant {i}: {solution.path} {solution.path_length}")
+    #     logger.debug(f"best solution: {self.best_solution.path} {self.best_solution.path_length}")
+    #     logger.debug(f"best path length: {self.best_path_length}")
+    #     logger.debug("")
+    #     return pheromone
     
     @timeit
     def generate_solution(self, instance: Instance, k_factor: KFactor, has_closed_cycle: bool):
@@ -102,8 +118,7 @@ class AntColonyOptimization(Model):
         pheromone = np.full((instance.number_of_edges, instance.number_of_edges), self.initial_pheromone)
 
         for _ in range(self.iterations):
-            solutions = self._generate_solutions(instance, pheromone)
+            solutions = self._generate_solutions(instance, pheromone, k_factor, has_closed_cycle)
             self._update_pheromone(pheromone, solutions)
         
         return self.best_solution
-
--- test.py
+++ test.py
@@ -1,3 +1,49 @@
 #%%
-from k_tsp_solver import Experiment, ModelName, KFactor, export_results_by_instance, SELECTED_INSTANCES
+from k_tsp_solver import Instance, Experiment, ModelName, KFactor, export_results_by_instance, SELECTED_INSTANCES
+import numpy as np
+import random
+
+random.seed(0)
+# %%
+instance = Instance(name="att48.tsp")
+instance.get_instance()
+# %%
+initial_pheromone = 1.0
+beta = 0.3
+alpha = 0.7
+# %%
+pheromone = np.full((instance.number_of_vertices, instance.number_of_vertices), initial_pheromone)
+# %%
+# generate solutions
+## for loop for each ant
+### call generate_solution
+path = []
+visited = set()
+first_edge = random.randint(1, instance.number_of_vertices)
+visited.add(first_edge)
+current = first_edge
+path.append(current)
+# %%
+path
+# %%
+# construct path loop
+## _select_next_node
+### _calculate_probabilities
+
+def _calculate_probability(instance: Instance, current: int, next_node: int, pheromone: np.ndarray) -> float:
+    pheromone_factor = pheromone[current - 1, next_node - 1] ** alpha
+    distance_factor = 1 / instance.graph.get_edge_data(current, next_node)["weight"] ** beta
+    return pheromone_factor * distance_factor
+
+def _calculate_probabilities(instance: Instance, current: int, visited: set, pheromone: np.ndarray) -> np.ndarray:
+        unvisited = instance.graph.nodes - visited
+        probabilities = np.zeros(instance.number_of_vertices)
+        for node in unvisited:
+            probabilities[node - 1] = _calculate_probability(instance, current, node, pheromone)
+        return probabilities / np.sum(probabilities)
+
+probabilities = _calculate_probabilities(instance, current, visited, pheromone)
+# %%
+# select next node
+np.random.choice(instance.number_of_vertices, p=probabilities)
 # %%
